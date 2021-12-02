b08b9557fbfebdb54d88c9a490c13593e9efccc0
Daniel Echeverri <53094888+daechepe@users.noreply.github.com>
2021-12-02 18:57:24 -0500

    Se agregan solución al sprint 3

--- src.js
+++ src.js
@@ -1,11 +1,7 @@
-const calcularPocentajes = async (
-    valorCO,
-    valorCO2,
-    valorHC,
-    valorO2
-) => {
 
-    let response = await fetch( "https://misiontic2022upb.vercel.app/api/emission-measurement/limits");
+const calcularPorcentajes = async (valorCO, valorCO2, valorHC, valorO2) => {
+
+    let response = await fetch('https://misiontic2022upb.vercel.app/api/emission-measurement/limits');
     let limits = await response.json();
 
     let limiteCO = limits['Parametro CO en rango estandar'];
@@ -18,62 +14,84 @@ const calcularPocentajes = async (
     let porcentajeHC = limiteHC ? (valorHC*100)/limiteHC : 0;
     let porcentajeO2 = limiteO2 ? (valorO2*100)/limiteO2 : 0;
 
-    return {
-    porcentajeCO,
-    porcentajeCO2,
-    porcentajeHC,
-    porcentajeO2,
-    };
-};
+    return {porcentajeCO, porcentajeCO2, porcentajeHC, porcentajeO2};
+}
 
 const registrarCO = async (value) => {
-    let response = await fetch( "https://misiontic2022upb.vercel.app/api/emission-measurement/ranges-parameters");
+    let response = await fetch('https://misiontic2022upb.vercel.app/api/emission-measurement/ranges-parameters');
     let rangosCO = await response.json();
-    let candidate = rangosco[0]
-    /*
+    let candidate = rangosCO[0];
     let resultado = 'fuera_de_rango';
-    global.rangosCO.filter(function(element){
-        if(value>=element.de && value<=element.hasta){
-            resultado = element.etiqueta
-        }
-    })
-    return resultado; */
-};
+
+    if(value>=candidate.de && value<=candidate.hasta){
+        resultado = candidate.etiqueta
+    }
+    
+    return resultado;
+    }
+
 const registrarCO2 = async (value) => {
-    let response = await fetch( "https://misiontic2022upb.vercel.app/api/emission-measurement/ranges-parameters");
-    /*  let resultado = 'fuera_de_rango';
-    global.rangosCO2.filter(function(element){
-        if(value>=element.de && value<=element.hasta){
-            resultado = element.etiqueta
-        }
-    })
-    return resultado; */
+    let response = await fetch('https://misiontic2022upb.vercel.app/api/emission-measurement/ranges-parameters');
+    let rangosCO2 = await response.json();
+    let candidate = rangosCO2[1];
+    let resultado = 'fuera_de_rango';
 
-};
+    if(value>=candidate.de && value<=candidate.hasta){
+        resultado = candidate.etiqueta
+    }
+
+    return resultado;
+    }
+
 const registrarHC = async (value) => {
-    let response = await fetch( "https://misiontic2022upb.vercel.app/api/emission-measurement/ranges-parameters");
-    /*  let resultado = 'fuera_de_rango';
-    global.rangosHC.filter(function(element){
-        if(value>=element.de && value<=element.hasta){
-            resultado = element.etiqueta
-        }
-    })
-    return resultado; */
+    let response = await fetch('https://misiontic2022upb.vercel.app/api/emission-measurement/ranges-parameters');
+    let rangosHC = await response.json();
+    let candidate = rangosHC[2];
+    let resultado = 'fuera_de_rango';
 
-};
+    if(value>=candidate.de && value<=candidate.hasta){
+        resultado = candidate.etiqueta
+    }
+
+    return resultado;
+    }
+
 const registrarO2 = async (value) => {
-    let response = await fetch( "https://misiontic2022upb.vercel.app/api/emission-measurement/ranges-parameters" );
-    /* let resultado = 'fuera_de_rango';
-    global.rangosO2.filter(function(element){
-        if(value>=element.de && value<=element.hasta){
-            resultado = element.etiqueta
-        }
-    })
-    return resultado;*/
-};
+    let response = await fetch('https://misiontic2022upb.vercel.app/api/emission-measurement/ranges-parameters');
+    let rangosO2 = await response.json();
+    let candidate = rangosO2[3];
+    let resultado = 'fuera_de_rango';
+
+    if(value>=candidate.de && value<=candidate.hasta){
+        resultado = candidate.etiqueta
+    }
+
+    return resultado;
+    }
+
+//global {}
+/* Sprint 2
+global.rangosCO = [
+    { etiqueta: 'Parametro CO en rango estandar', de: 0, hasta: 10 },
+    { etiqueta: 'Parametro CO fuera de rango', de: 11, hasta: 15 },
+]
+
+global.rangosCO2 = [
+    { etiqueta: 'Parametro CO2 en rango estandar', de: 0, hasta: 20 },
+    { etiqueta: 'Parametro CO2 fuera de rango', de: 21, hasta: 30 },
+]
+global.rangosHC = [
+    { etiqueta: 'Parametro HC en rango estandar', de: 0, hasta: 10000 },
+    { etiqueta: 'Parametro HC fuera de rango', de: 10001, hasta: 11000 },
+]
+global.rangosO2 = [
+    { etiqueta: 'Parametro O2 en rango estandar', de: 0, hasta: 22 },
+    { etiqueta: 'Parametro O2 fuera de rango', de: 23, hasta: 30 },
+]
+*/
 
 module.exports.registrarCO = registrarCO;
 module.exports.registrarCO2 = registrarCO2;
 module.exports.registrarHC = registrarHC;
 module.exports.registrarO2 = registrarO2;
-module.exports.calcularPocentajes = calcularPocentajes;
+module.exports.calcularPorcentajes = calcularPorcentajes;
