25ba082b0355d3bb0d75169c60f8ec2f4b5e300a
Ela Pino <90581426+Elapino@users.noreply.github.com>
2021-12-03 17:53:43 -0500

    Update SRC.js

--- SRC.js
+++ SRC.js
@@ -3,13 +3,16 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.get("tu ruta aquí ", (req, res) => {
+  ///api/emissionmeasurement/ranges_parameters
   //manejador paa retornar los rangos
 });
 app.post("tu ruta aquí", (req, res) => {
+  //api/emissionmeasurement/ranges_parameters
   //range creation
 });
 app.get(
   "tu ruta aquí",
+  //api/emission-measurement/limits
   //retornar los límites
 );
 module.exports = app;
