50a2b45efe15dbd3d7a94ee550c84599145802e5
Daniel Osorio <63620989+osorio9121@users.noreply.github.com>
2021-12-08 12:12:01 -0500

    Update src.js
    Solución en ruta relativa y en el 3er método

--- src.js
+++ src.js
@@ -3,19 +3,19 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("api/emissionmeasurement/ranges_parameters", (req, res) => {
+app.get("/api/emissionmeasurement/ranges_parameters", (req, res) => {
     res.json([ranges_parameters]);
 });
 
-app.post("api/emissionmeasurement/ranges_parameters", (req, res) => {
+app.post("/api/emissionmeasurement/ranges_parameters", (req, res) => {
     const newRange = req.body;
     ranges_parameters.push(newRange)
     res.json([ranges_parameters]);
 });
 
-app.get(
-    "api/emission-measurement/limits",
-    res.json([limits]) ); 
+app.get("/api/emission-measurement/limits", (req, res) => {
+    res.json([limits]); 
+}); 
 
 
 module.exports = app;
